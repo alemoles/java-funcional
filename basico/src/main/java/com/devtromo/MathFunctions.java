5992eb42bdd03fad54aa4436bd15ee4d91656149
alemoles <alemolerio@gmail.com>
2022-12-04 23:28:08 -0300

    Predicate in java 8

--- basico/src/main/java/com/devtromo/MathFunctions.java
+++ basico/src/main/java/com/devtromo/MathFunctions.java
@@ -1,6 +1,7 @@
 package com.devtromo;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
     public static void main(String[] args) {
@@ -11,7 +12,28 @@ public class MathFunctions {
             }
         };
 
-        int number = square.apply(10);
-        System.out.println(number);
+        int result = square.apply(10);
+        System.out.println(result);
+
+        Function<Integer, Boolean> isOdd = number -> number % 2 == 1;
+        Predicate<Integer> isEven = number -> number % 2 == 0;
+
+        Predicate<Student> isApproved = student -> student.getCalificacion() >= 6.0;
+        Student someone = new Student(5.9);
+        System.out.println(isApproved.test(someone));
+
+        isEven.test(4); // true
+    }
+
+    static class Student {
+        private final double calificacion;
+
+        public Student(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
     }
 }
