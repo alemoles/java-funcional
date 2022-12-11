c1f2b7f78f78cbf0a04cbfe7f710b1630050ac75
alemoles <alemolerio@gmail.com>
2022-12-11 11:46:04 -0300

    Collectors in Java 8

--- basico/src/main/java/com/devtromo/streams/TypeStream.java
+++ basico/src/main/java/com/devtromo/streams/TypeStream.java
@@ -1,15 +1,17 @@
 package com.devtromo.streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
 
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
-        infiniteStream.limit(1000)
+        List<Integer> list = infiniteStream.limit(1000)
             .filter(x -> x % 2 == 0)
             .boxed()
-            .forEach(System.out::println);
+            .collect(Collectors.toList());
 
     }
 }
